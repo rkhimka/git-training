4ec0fae9a2e3cfd15376092a9b0c09b5d07ae2b6
rkhimka <romkhimka@gmail.com>
2021-01-19 21:00:06 +0200

    added new line in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car(240, "Toyota", "red");
         System.out.println("Car --> "+car.getCarName() +" and color -> " + car.getColor());
+        System.out.println("new change");
     }
 }
