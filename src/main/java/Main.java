c37faa68b44a97fa01d63757c2f56a170770bf47
rkhimka <romkhimka@gmail.com>
2021-01-19 22:40:19 +0200

    added new message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car(240, "Toyota", "red");
         System.out.println("Car --> "+car.getCarName() +" and color -> " + car.getColor());
-        System.out.println("new change");
-        Tree.getClassType();
+        System.out.println("added new message");
     }
 }
