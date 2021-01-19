e8156d4d1327a0c7b53deb26b21b9635e6063dc8
rkhimka <romkhimka@gmail.com>
2021-01-19 22:10:58 +0200

    after cherry pick

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car(240, "Toyota", "red");
         System.out.println("Car --> "+car.getCarName() +" and color -> " + car.getColor());
+
+        //YAHOO
         System.out.println("added new message");
     }
 }
